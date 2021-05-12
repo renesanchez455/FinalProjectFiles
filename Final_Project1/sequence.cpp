82dc99cced698d07a0da35b992c00c8075be57db
xBubblesx <84062055+xBubblesx@users.noreply.github.com>
2021-05-12 01:53:02 -0600

    Update sequence.cpp

--- Final_Project1/sequence.cpp
+++ Final_Project1/sequence.cpp
@@ -1,5 +1,6 @@
 #include "sequence.h"
 
+//Initializes the dequence constructor
 Sequence::Sequence(const QString &sId, const QString &prog, const QString &progY, const QString &sem, const QString &cId)
 {
     syllabusId = sId;
@@ -8,51 +9,64 @@ Sequence::Sequence(const QString &sId, const QString &prog, const QString &progY
     Semester = sem;
     courseId = cId;
 }
+
+//Sets the course ID
 void Sequence::setcourseId(QString crsID)
 {
     courseId = crsID;
 }
 
+//Sets the syllabus ID
 void Sequence::setsyllabusId(QString syllID)
 {
     syllabusId = syllID;
 }
 
+
+//Sets the program
 void Sequence::setProgram(QString Prog)
 {
     Program = Prog;
 }
 
+
+//Sets the program year
 void Sequence::setprogramYear(QString progrY)
 {
     programYear = progrY;
 }
 
+//Sets the semester
 void Sequence::setSemester(QString Smstr)
 {
     Semester = Smstr;
 }
 
+//Return the course ID
 QString Sequence::getcourseId()
 {
     return courseId;
 }
 
+//Return the syllabus ID
 QString Sequence::getsyllabusId()
 {
     return syllabusId;
 }
 
+//Return the program
 QString Sequence::getProgram()
 {
     return Program;
 }
 
+//Return the program year
 QString Sequence::getprogramYear()
 {
     return programYear;
 }
 
+//Return the semester
 QString Sequence::getSemester()
 {
     return Semester;
