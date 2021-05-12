d4dbb56485945d75c4de21291830df6c7d83050b
xBubblesx <84062055+xBubblesx@users.noreply.github.com>
2021-05-12 01:36:30 -0600

    Update reset.cpp

--- Final_Project1/reset.cpp
+++ Final_Project1/reset.cpp
@@ -1,5 +1,6 @@
 #include "reset.h"
 
+//Initializes the reset constructor
 Reset::Reset(const QString &uId, const QString &nPwd, const QString &tp)
 {
     userId = uId;
@@ -7,31 +8,37 @@ Reset::Reset(const QString &uId, const QString &nPwd, const QString &tp)
     type = tp;
 }
 
+//Sets the user ID
 void Reset::setuserId(QString Uid)
 {
     userId = Uid;
 }
 
+//Sets the new password
 void Reset::setnewPwd(QString newP)
 {
     newPwd = newP;
 }
 
+//Sets the type
 void Reset::setType(QString t)
 {
     type = t;
 }
 
+//Returns the user ID
 QString Reset::getuserId()
 {
     return userId;
 }
 
+//Returns the new password
 QString Reset::getnewPwd()
 {
     return newPwd;
 }
 
+//Returns the type
 QString Reset::getType()
 {
     return type;
