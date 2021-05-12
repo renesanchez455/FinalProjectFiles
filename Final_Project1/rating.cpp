cc4cebb0fe517412be4b2df782eb56806f068dcd
xBubblesx <84062055+xBubblesx@users.noreply.github.com>
2021-05-12 01:33:49 -0600

    Update rating.cpp

--- Final_Project1/rating.cpp
+++ Final_Project1/rating.cpp
@@ -1,7 +1,7 @@
 #include "rating.h"
 
 
-
+//Initializes the rating constructor
 Rating::Rating(const QString &cId, const QString &lId, const QString &ufn, const QString &diff, const QString &cTpe, const QString &enjblty, const QString &yr, const QString &cmmnt)
 {
     courseId = cId;
@@ -14,79 +14,99 @@ Rating::Rating(const QString &cId, const QString &lId, const QString &ufn, const
     comment = cmmnt;
 }
 
+//Sets the course ID
 void Rating::setCourse(QString CID)
 {
     courseId = CID;
 }
 
+//Sets the lecturer ID
 void Rating::setLecturer(QString LID)
 {
     lecturerId = LID;
 
 }
+
+//Sets the course usefulness
 void Rating::setUsefulness(QString UFN)
 {
     usefulness = UFN;
 }
+
+//Sets the course difficulty
 void Rating::setDifficulty(QString DIFF)
 {
     difficulty = DIFF;
 }
+
+//Sets the course type
 void Rating::setCourseType(QString CTPE)
 {
     courseType = CTPE;
 }
 
+//Sets the course enjoyability
 void Rating::setEnjoyability(QString ENJ)
 {
     enjoyability = ENJ;
 }
 
+//Sets the year
 void Rating::setYear(QString YR)
 {
     year = YR;
 }
 
+//Sets the comment
 void Rating::setComment(QString CMMNT)
 {
     comment = CMMNT;
 }
 
+//Returns the course ID
 QString Rating::getCourse()
 {
     return courseId;
 }
 
+//Returns the lecturer ID
 QString Rating::getLecturer()
 {
     return lecturerId;
 }
 
+//Returns the course usefulness
 QString Rating::getUsefulness()
 {
     return usefulness;
 }
 
+//Returns the course difficulty
 QString Rating::getDifficulty()
 {
     return difficulty;
 }
 
+//Returns the course type
 QString Rating::getCourseType()
 {
     return courseType;
 }
 
+//Returns the course enjoyability
 QString Rating::getEnjoyability()
 {
     return enjoyability;
 }
 
+
+//Returns the year
 QString Rating::getYear()
 {
     return year;
 }
 
+//Returns the comment
 QString Rating::getComment()
 {
     return comment;
