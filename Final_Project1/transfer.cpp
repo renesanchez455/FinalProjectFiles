bffb22fff6d6f602c1d5c4149d0d99e829500bb3
xBubblesx <84062055+xBubblesx@users.noreply.github.com>
2021-05-12 13:28:22 -0600

    Update transfer.cpp

--- Final_Project1/transfer.cpp
+++ Final_Project1/transfer.cpp
@@ -1,6 +1,6 @@
 #include "transfer.h"
 
-
+//Initializes the transfer constructor
 Transfer::Transfer(const QString &cId, const QString &grd, const QString &inst)
 {
     courseId = cId;
@@ -8,31 +8,37 @@ Transfer::Transfer(const QString &cId, const QString &grd, const QString &inst)
     institution = inst;
 }
 
+//Set the course ID
 void Transfer::setCourse(QString CID)
 {
     courseId = CID;
 }
 
+//Set the grade
 void Transfer::setGrade(QString GRD)
 {
     grade = GRD;
 }
 
+//Set the institution
 void Transfer::setInstitution(QString INST)
 {
     institution = INST;
 }
 
+//Returns the course ID
 QString Transfer::getCourse()
 {
     return courseId;
 }
 
+//Returns the grade
 QString Transfer::getGrade()
 {
     return grade;
 }
 
+//Returns the institution
 QString Transfer::getInstitution()
 {
     return institution;
