b0c80a2e7fb2c6a43f735c70bd5be08419ab8834
xBubblesx <84062055+xBubblesx@users.noreply.github.com>
2021-05-12 00:49:31 -0600

    Update lecturer.cpp

--- Final_Project1/lecturer.cpp
+++ Final_Project1/lecturer.cpp
@@ -1,5 +1,6 @@
 #include "Lecturer.h"
 
+//Initializes lecturer constructor
 Lecturer::Lecturer(const QString &id, const QString &fName, const QString &lName, const QString &pw, const QString &email, const QString &pNo, const QString &gender, const QString &birthDate)
     :User(id, fName, lName, pw, email, pNo, gender, birthDate)
 {
